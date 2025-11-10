88a8e27a6b209b4baff834666262cffe8d0a9ca5
Devendra Agrawal <155873104+DARSH-07@users.noreply.github.com>
2025-11-10 15:00:56 +0530

    Delete core directory

--- core/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import consumers
-
-websocket_urlpatterns = [
-    path('ws/interview/', consumers.InterviewConsumer.as_asgi()),
-    path('ws/dashboard/', consumers.DashboardConsumer.as_asgi()),
-]
